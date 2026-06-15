c6b2b485c784344fa9e374872707e1fe4f8bb977
Dionysis <dionysisterziou@gmail.com>
2026-06-15 20:11:53 +0300

    feat: render landing page template

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def home():
-    return {"message": "Local Request Manager is running"}
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+    )
